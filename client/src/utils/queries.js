379b5270d6787c76417f28e9a7c85670c0bcebd3
J0J0 <iditaroddream@gmail.com>
2023-04-01 20:05:44 -0500

    queries

---
+++ client/src/utils/queries.js
@@ -0,0 +1,18 @@
+import {gql} from '@apollo/client';
+
+export const getItem = gql`
+{
+    me{
+        _id
+        username
+        email
+        saved_books{
+            bookId
+            authors
+            description
+            title
+            impage
+            link
+        }
+    }
+}`
